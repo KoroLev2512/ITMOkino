c68aedf570810e6bba1b924981656bdec9a7cc61
Mikhail <90066666+mip3x@users.noreply.github.com>
2025-03-02 23:23:19 +0300

    feat: npm run dev in background

--- jenkins/frontend-ci.groovy
+++ jenkins/frontend-ci.groovy
@@ -12,7 +12,7 @@ pipeline {
             steps {
                 sh 'npm install'
                 sh 'npm run build'
-                sh 'npm run dev'
+                sh 'nohup npm run dev &'
             }
         }
     }
