7a3e3b0cf3095cab93dfdfb87f14728083ff4f64
Mikhail <90066666+mip3x@users.noreply.github.com>
2025-03-02 23:05:51 +0300

    feat: frontend-ci.groovy added

---
+++ jenkins/frontend-ci.groovy
@@ -0,0 +1,17 @@
+pipeline {
+    agent { label 'local-agent' }
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: '${branch}',
+                    credentialsId: 'github',
+                    url: 'git@github.com:KoroLev2512/ITMO-kino.git'
+            }
+        }
+        stage('Setup frontend dev server') {
+            steps {
+                sh 'test message'
+            }
+        }
+    }
+}
