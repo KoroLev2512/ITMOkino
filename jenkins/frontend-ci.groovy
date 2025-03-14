6217393276dba5ffd71726553a97ca322e48a9ce
Mikhail <90066666+mip3x@users.noreply.github.com>
2025-03-14 15:45:02 +0300

    Update frontend-ci.groovy

--- jenkins/frontend-ci.groovy
+++ jenkins/frontend-ci.groovy
@@ -1,11 +1,16 @@
 pipeline {
     agent any
+
+    environment {
+        HOST = '127.0.0.1'
+    }
+    
     stages {
         stage('Checkout') {
             steps {
                 git branch: 'main',
                     credentialsId: 'github',
-                    url: 'git@github.com:KoroLev2512/ITMO-kino.git'
+                    url: 'git@github.com:KoroLev2512/ITMOkino.git'
             }
         }
         stage('Check current state') {
@@ -15,15 +20,26 @@ pipeline {
         }
         stage('Stop previous dev-server') {
             steps {
-                sh 'pm2 stop dev-server || true'
-                sh 'pm2 delete dev-server || true'
+                sh 'pm2 stop itmo-kino || true'
+                sh 'pm2 delete itmo-kino || true'
             }
         }
-        stage('Setup frontend dev-server') {
+        stage('Update node version && setup frontend server') {
             steps {
-                sh 'npm install'
-                sh 'npm run build'
-                sh 'pm2 start npm --name "dev-server" -- run dev'
+                sh '''#!/bin/bash
+                echo "=== Before sourcing nvm ==="
+                node --version || true
+
+                source ~/.nvm/nvm.sh
+                nvm use 22.14.0
+
+                echo "=== After nvm use 22.14.0 ==="
+                node --version
+                
+                npm i
+                npm run build
+                pm2 start npm --name "itmo-kino" -- start
+                '''
             }
         }
     }
